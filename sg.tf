8d161d1f62222bf906970e655182817b83c8e958
cg-dv <cgdevproject@gmail.com>
2021-08-22 16:30:34 -0700

    Added RDS security group

--- sg.tf
+++ sg.tf
@@ -19,6 +19,27 @@ resource "aws_security_group" "http" {
   }
 }
 
+resource "aws_security_group" "rds" {
+  name        = "rds-db-sg"
+  description = "Allow MySQL inbound traffic from web servers"
+  vpc_id      = aws_vpc.example.id
+
+  ingress {
+    description     = "MySQL from Web SG"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.http.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "vpc-endpoint-secrets-manager" {
   name        = "vpc-endpoint-secretsmanager"
   description = "Allow access to secrets manager from EC2"
