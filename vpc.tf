abfa596b5d0c171df1edbfe73cb0f2ca93983c3f
cg-dv <cgdevproject@gmail.com>
2021-08-22 16:33:18 -0700

    Added private subnets to VPC, added route tables for public and private subnets, added route table associations for public subnets

--- vpc.tf
+++ vpc.tf
@@ -30,12 +30,12 @@ resource "aws_eip" "NAT-eip" {
 
 resource "aws_default_route_table" "default-private-route-table" {
   default_route_table_id = aws_vpc.example.default_route_table_id
-  
+
   route = [
-  {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.public-NAT-gw.id
-  }
+    {
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = aws_nat_gateway.public-NAT-gw.id
+    }
   ]
 }
 
@@ -43,22 +43,44 @@ resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.example.id
 
   route = [
-  {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+    {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igw.id
+    }
   ]
 }
 
-resource "aws_subnet" "example_subnet_1" {
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_subnet" "public_subnet_1" {
   vpc_id               = aws_vpc.example.id
   cidr_block           = "10.0.1.0/24"
   availability_zone_id = "usw1-az1"
 }
 
-resource "aws_subnet" "example_subnet_2" {
+resource "aws_subnet" "public_subnet_2" {
   vpc_id               = aws_vpc.example.id
   cidr_block           = "10.0.2.0/24"
+  availability_zone_id = "usw1-az3"
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id               = aws_vpc.example.id
+  cidr_block           = "10.0.3.0/24"
+  availability_zone_id = "usw1-az1"
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id               = aws_vpc.example.id
+  cidr_block           = "10.0.4.0/24"
   availability_zone_id = "usw1-az3"
 }
 
@@ -66,7 +88,7 @@ resource "aws_vpc_endpoint" "secrets-manager-endpoint" {
   vpc_id              = aws_vpc.example.id
   service_name        = "com.amazonaws.us-west-1.secretsmanager"
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.example_subnet_1.id, aws_subnet.example_subnet_2.id]
+  subnet_ids          = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   private_dns_enabled = true
   security_group_ids  = [aws_security_group.vpc-endpoint-secrets-manager.id]
 }
