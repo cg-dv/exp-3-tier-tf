8dd81a97844276989d602b9a749dc0059be48cc5
cg-dv <cgdevproject@gmail.com>
2021-08-22 12:10:24 -0700

    Updated RDS and Secrets Manager to correctly reference username in Secrets Manager secret

--- rds.tf
+++ rds.tf
@@ -5,29 +5,11 @@ resource "aws_db_instance" "example_rds" {
   engine                = "mysql"
   engine_version        = "8.0.23"
   name                  = "example_db"
-  username              = local.db_credentials.user
+  username              = local.db_credentials.username
   password              = local.db_credentials.password
   parameter_group_name  = "default.mysql8.0"
   skip_final_snapshot   = true
   publicly_accessible   = true
   multi_az              = false
   snapshot_identifier   = "arn:aws:rds:us-west-1:414402433373:snapshot:snapshot-07-13-2021"
-}
-
-output "rds_id_endpoint" {
-  value       = aws_db_instance.example_rds.endpoint
-  description = "Endpoint of RDS instance."
-
-  depends_on = [
-    aws_db_instance.example_rds
-  ]
-}
-
-output "rds_id" {
-  value       = aws_db_instance.example_rds.id
-  description = "Id of RDS instance."
-
-  depends_on = [
-    aws_db_instance.example_rds
-  ]
 }
--- secrets_manager.tf
+++ secrets_manager.tf
@@ -3,7 +3,7 @@ resource "aws_secretsmanager_secret_version" "new_secret_version" {
   secret_string = jsonencode(
     {
       "host" : aws_db_instance.example_rds.endpoint,
-      "username" : local.db_credentials.user,
+      "username" : local.db_credentials.username,
       "password" : local.db_credentials.password,
       "database" : local.db_credentials.database
     }
