59ee0dbbc5e3f594516953c8d23ce398ed32cdc9
cg-dv <cgdevproject@gmail.com>
2021-08-22 16:36:01 -0700

    Added db subnet group name and VPC security group ids to RDS config

--- rds.tf
+++ rds.tf
@@ -1,15 +1,27 @@
+resource "aws_db_subnet_group" "mysql-db-subnets" {
+
+  name       = "mysql-rds-subnet-group"
+  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+
+  tags = {
+    Name = "RDS"
+  }
+}
+
 resource "aws_db_instance" "example_rds" {
-  allocated_storage     = 20
-  max_allocated_storage = 30
-  instance_class        = "db.t3.micro"
-  engine                = "mysql"
-  engine_version        = "8.0.23"
-  name                  = "example_db"
-  username              = local.db_credentials.username
-  password              = local.db_credentials.password
-  parameter_group_name  = "default.mysql8.0"
-  skip_final_snapshot   = true
-  publicly_accessible   = true
-  multi_az              = false
-  snapshot_identifier   = "arn:aws:rds:us-west-1:414402433373:snapshot:snapshot-07-13-2021"
+  allocated_storage      = 20
+  max_allocated_storage  = 30
+  instance_class         = "db.t3.micro"
+  engine                 = "mysql"
+  engine_version         = "8.0.23"
+  name                   = "example_db"
+  username               = local.db_credentials.username
+  password               = local.db_credentials.password
+  db_subnet_group_name   = aws_db_subnet_group.mysql-db-subnets.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = "default.mysql8.0"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+  multi_az               = false
+  snapshot_identifier    = "arn:aws:rds:us-west-1:414402433373:snapshot:snapshot-07-13-2021"
 }
